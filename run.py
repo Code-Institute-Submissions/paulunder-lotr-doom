4642595cc890f91962b4c0caf4f42467ebcc29b7
paul <paul@edda.work>
2024-03-30 18:23:44 +0100

    added env variables with creds

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import random
 import gspread
 from google.oauth2.service_account import Credentials
 import json
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,13 +13,15 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+creds_json = os.environ.get('CREDS')
+CREDS = json.loads(creds_json)
+
+
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('LordOfTheRings')
 
 auth_sheet = SHEET.worksheet('Auth')
-quiz_sheet = SHEET.worksheet('Quiz')
 
 
 # Function to register a new user
